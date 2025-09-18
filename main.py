36e131c40f4304d0fcbe2bd3d433a81c2cdc28b5
DiandreRuiz <Diandreruiz77@gmail.com>
2025-09-18 11:28:48 -0400

    Started planning

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+To-Do App
+
+Requirements:
+- Welcomes user
+- Display menu with options for tasks and app:
+    - add
+    - view
+    - delete
+    - quit app
+- Tasks stored in Python List
+
+
+
+"""
